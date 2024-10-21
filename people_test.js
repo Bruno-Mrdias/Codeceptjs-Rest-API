febaccd37df4fc7e7930c9f618c261b0ff7e8c5a
Bruno Matheus Ribeiro <brunomatheus168@gmail.com>
2024-10-21 20:42:49 -0300

    first commit

---
+++ people_test.js
@@ -0,0 +1,43 @@
+Feature('people');
+
+Scenario('Teste validar request ',  ({ I }) => {
+
+    I.sendGetRequest('/people')
+    I.seeResponseCodeIsSuccessful()
+
+});
+
+Scenario('Teste validar request people 1 ',  ({ I }) => {
+
+    I.sendGetRequest('/people/1')
+    I.seeResponseCodeIsSuccessful()
+    I.seeResponseContainsKeys(['name', 'height'])
+    I.seeResponseContainsJson({
+        name: 'Luke Skywalker',
+        height: '172',
+        birth_year: '19BBY',
+        gender: 'male'
+    })
+
+});
+
+Scenario('Teste validar request people 3 ',  ({ I }) => {
+
+    I.sendGetRequest('/people/3')
+    I.seeResponseCodeIsSuccessful()
+    I.seeResponseContainsJson({
+        name: 'R2-D2',
+        eye_color: 'red',
+        height: '96'
+    })
+
+});
+
+Scenario('Teste validar request people 2 ',  ({ I }) => {
+
+    I.sendGetRequest('/people/2')
+    I.seeResponseCodeIsSuccessful()
+    I.seeResponseContainsKeys(['name', 'height', 'eye_color'])
+
+});
+
